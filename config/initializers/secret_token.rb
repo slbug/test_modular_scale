e60e5fa05f71d2fb6fe1f13a82c4ae99c4b712ba
bUg <aleks.grebennik@gmail.com>
2012-09-05 14:27:52 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+TestModularScale::Application.config.secret_token = '9fbb1b8233af6b4b6c6623b6f93c216d5fba6d0e2c70e57a25849f9d921ddd2071ec85671b98a1916aca8c2d08a03d4f6529dc57a714101c2ff163086145746c'
